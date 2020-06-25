326038eae21392d158462159c70b08cf348939dc
Greg Lucas <greg.lucas@lasp.colorado.edu>
2020-06-25 11:21:37 -0600

    Adding a gridded magnetic field animation and a timeline to the animation plot.

--- code/plot_figXX_Efields.py
+++ code/plot_figXX_Efields.py
@@ -193,26 +193,30 @@ def plot_E3_Efield_map_sites(ax1, ax2, B_sites, E_sites):
 def animate_fields(E_sites, ts, fs):
     from matplotlib import animation
     # Set up the figure and axes
-    fig = plt.figure(figsize=(6.5, 6.5))
-    gs = fig.add_gridspec(ncols=1, nrows=2, height_ratios=[1, 1])
-    ax1 = fig.add_subplot(gs[0], projection=projection)
-    ax2 = fig.add_subplot(gs[1], projection=projection)
-    plt.subplots_adjust(left=0.11, right=0.99, top=0.9, bottom=0.05)
+    fig = plt.figure(figsize=(10, 5))
+    height_ratios = [4, 10, 1]
+    gs = fig.add_gridspec(ncols=3, nrows=3, height_ratios=height_ratios,
+                          hspace=0.5)
+    ax_time = fig.add_subplot(gs[0, :])
+    ax_bfield = fig.add_subplot(gs[1, 0], projection=projection)
+    ax_bfield_cbar = fig.add_subplot(gs[2, 0])
+    ax_efield = fig.add_subplot(gs[1, 1], projection=projection)
+    ax_efield_cbar = fig.add_subplot(gs[2, 1])
+    ax_voltage = fig.add_subplot(gs[1, 2], projection=projection)
+    ax_voltage_cbar = fig.add_subplot(gs[2, 2])
 
-    # Set up the initial quiver plots
-    pred_lons = MT_xys[:, 1]
-    pred_lats = MT_xys[:, 0]
-
-    Ex = E_sites[:, :, 0]
-    Ey = E_sites[:, :, 1]
-
-    # Force all E's to 0 at t=0 to avoid plotting the Gibb's ringing
-    Ex[0, :] = 0
-    Ey[0, :] = 0
-
-    # Log-scale vectors immediately?
-    # Bx, By = symlog(Bx, By)
-    # Ex, Ey = symlog(Ex, Ey)
+    # Time series
+    # -----------
+    ax = ax_time
+    B_E3A = Bt_E3A(ts)
+    B_E3B = Bt_E3B(ts)
+    ax_time.plot(ts, B_E3A)
+    ax_time.plot(ts, B_E3B)
+    ax_time.plot(ts, B_E3A + B_E3B)
+    time_line = ax_time.axvline(0, c='k')
+    ax_time.set_xlim(ts[0], ts[-1])
+    ax_time.xaxis.tick_top()
+    ax_time.tick_params(direction='in', pad=0)
 
     # create grid for map
     enhance = 20
@@ -222,7 +226,9 @@ def animate_fields(E_sites, ts, fs):
                             nlon*enhance)
     grid_lats = np.linspace(lat_bounds[0]-2, lat_bounds[1]+2,
                             nlat*enhance)
-    lon_mesh, lat_mesh = _mesh_grid(grid_lons, grid_lats)
+    # Specify the grid points and edges separately for plotting
+    lon_mesh, lat_mesh = np.meshgrid(grid_lons, grid_lats)
+    lon_edges, lat_edges = _mesh_grid(grid_lons, grid_lats)
 
     # generate gridded B-field maps
     B_E3A, B_E3B = calc_B(lat_mesh.ravel(), lon_mesh.ravel(), ts)
@@ -231,68 +237,89 @@ def animate_fields(E_sites, ts, fs):
     By = B[:, :, 1]
     Bh = np.sqrt(Bx**2 + By**2)
 
-    norm = LogNorm(10, 1100)
-    cax1 = ax1.pcolormesh(lon_mesh, lat_mesh,
-                          Bh[0, :].reshape(lat_mesh.shape),
-                          transform=proj_data,
-                          norm=norm,
-                          alpha=.5,
-                          linewidth=0)
-    cax1.set_edgecolor('face')
-    cb1 = plt.colorbar(cax1, ax=ax1, orientation='vertical')
-    cb1.set_label(label='$B_h$ (nT)', fontsize=12)
-    cb1.ax.tick_params(labelsize=12)
+    norm = LogNorm(1, 2500)
+    ax = ax_bfield
+    ax_cbar = ax_bfield_cbar
+    pcol = ax.pcolormesh(lon_edges, lat_edges,
+                         Bh[0, :].reshape(lat_mesh.shape),
+                         transform=proj_data,
+                         norm=norm,
+                         alpha=.5,
+                         linewidth=0)
+    pcol.set_edgecolor('face')
+    cb = plt.colorbar(pcol, cax=ax_cbar, orientation='horizontal')
+    cb.set_label(label='$B_h$ (nT)', fontsize=12)
+    cb.ax.tick_params(labelsize=12)
 
     # Separately calculate the quiver points to make it a smaller grid
     grid_lons = np.linspace(lon_bounds[0]-2, lon_bounds[1]+2,
-                            nlon)
+                            nlon*2)
     grid_lats = np.linspace(lat_bounds[0]-2, lat_bounds[1]+2,
-                            nlat)
+                            nlat*2)
     lonq, latq = _mesh_grid(grid_lons, grid_lats)
     B_E3A, B_E3B = calc_B(latq.ravel(), lonq.ravel(), ts)
     Bq = B_E3A + B_E3B
     Bqx = Bq[:, :, 0]
     Bqy = Bq[:, :, 1]
 
-    quiv_B = ax1.quiver(lonq.ravel(), latq.ravel(),
-                        Bqy[0, :], Bqx[0, :],
-                        transform=proj_data,
-                        color='w',
-                        units='inches',
-                        scale=2500)
+    quiv_B = ax.quiver(lonq.ravel(), latq.ravel(),
+                       Bqy[0, :], Bqx[0, :],
+                       transform=proj_data,
+                       color='w',
+                       units='inches',
+                       scale=2500)
 
     # Red x marks the spot
-    ax1.scatter(lon_epi, lat_epi, color='r', marker='x',
-                s=50, transform=proj_data)
+    ax.scatter(lon_epi, lat_epi, color='r', marker='x',
+               s=50, transform=proj_data)
 
-    ax1.set_extent(plot_lon_bounds + lat_bounds, proj_data)
-    add_features_to_ax(ax1)
-    ax1.set_title('E3 B-field', fontsize=12)
+    ax.set_extent(plot_lon_bounds + lat_bounds, proj_data)
+    add_features_to_ax(ax)
+    ax.set_title('E3 B-field', fontsize=12)
 
+    # E-field
+    # -------
+    pred_lons = MT_xys[:, 1]
+    pred_lats = MT_xys[:, 0]
+
+    Ex = E_sites[:, :, 0]
+    Ey = E_sites[:, :, 1]
+
+    # Force all E's to 0 at t=0 to avoid plotting the Gibb's ringing
+    Ex[0, :] = 0
+    Ey[0, :] = 0
+    Eh = np.sqrt(Ex**2 + Ey**2)
+
+    ax = ax_efield
+    ax_cbar = ax_efield_cbar
     # Red x marks the spot
-    ax2.scatter(lon_epi, lat_epi, color='r', marker='x',
-                s=50, transform=proj_data)
+    ax.scatter(lon_epi, lat_epi, color='r', marker='x',
+               s=50, transform=proj_data)
     # Arrows for the data
-    quiv_E = ax2.quiver(pred_lons, pred_lats,
-                        Ey[0, :], Ex[0, :],
-                        transform=proj_data,
-                        color='w',
-                        units='inches',
-                        scale=10000)
+    quiv_E = ax.quiver(pred_lons, pred_lats,
+                       Ey[0, :], Ex[0, :],
+                       transform=proj_data,
+                       color='w',
+                       units='inches',
+                       scale=10000)
 
-    ax2.set_extent(plot_lon_bounds + lat_bounds, proj_data)
-    add_features_to_ax(ax2)
-    ax2.set_title('E3 E-field', fontsize=12)
+    ax.set_extent(plot_lon_bounds + lat_bounds, proj_data)
+    add_features_to_ax(ax)
+    ax.set_title('E3 E-field', fontsize=12)
 
     title = fig.suptitle('Time: 0 s')
 
     def animate(t):
-        cax1.set_array(Bh[t, :])
+        t *= 10
+        time_line.set_xdata(ts[t])
+        pcol.set_array(Bh[t, :])
         quiv_B.set_UVC(Bqy[t, :], Bqx[t, :])
         quiv_E.set_UVC(Ey[t, :], Ex[t, :])
-        title.set_text(f'Time: {t/fs:.2f} s')
+        title.set_text(f'Time: {ts[t]:.2f} s')
 
-    anim = animation.FuncAnimation(fig, animate, frames=[x*10 for x in range(250)], interval=10)
+    anim = animation.FuncAnimation(fig, animate,
+                                   frames=[x for x in range(250)],
+                                   interval=10)
     anim.save('../figs/test_animation.mp4')
 
 
@@ -328,7 +355,7 @@ def _mesh_grid(x, y):
 
 
 def main():
-    fs = 100  # sampling frequency
+    fs = 10  # sampling frequency
     ntimes = 1500000
     ntimes = 15000
     ts = np.arange(ntimes) / fs  # time steps
